e63f24f40e59e32c1f8a12453c83e323ba0ece62
Joakim Tysseng <joakim@tysseng.no>
2018-04-22 10:22:28 +0200

    ES6-ified

--- src/processing/imageProcessor.js
+++ src/processing/imageProcessor.js
@@ -1,54 +1,28 @@
 import jsfeat from 'jsfeat';
 
 const drawImageOnCanvas = (ctx) => {
-  const img=document.getElementById("sourceImage");
-  ctx.drawImage(img,0,0);
+  const img = document.getElementById("sourceImage");
+  ctx.drawImage(img, 0, 0);
 };
 
-function writeToGrayscaleImageData(image_data, img){
-  var data_u32 = new Uint32Array(image_data.data.buffer);
-  var i = img.cols*img.rows, pix = 0;
+const writeToGrayscaleImageData = (image_data, img) => {
+  const data_u32 = new Uint32Array(image_data.data.buffer);
+  const alpha = (0xff << 24);
 
-  var alpha = (0xff << 24);
-  while(--i >= 0) {
+  let i = img.cols * img.rows, pix = 0;
+  while (--i >= 0) {
     pix = img.data[i];
     data_u32[i] = alpha | (pix << 16) | (pix << 8) | pix;
   }
-}
+};
 
+const findBoundingCorners = (boundingBox, corners) => {
 
-function topCornerFilter(boundingBox){
-  return function (corner){
-    return corner.y === boundingBox.topLeft.y;
-  }
-}
+  let topLeft, topRight, bottomLeft, bottomRight;
 
-function leftCornerFilter(boundingBox){
-  return function (corner){
-    return corner.x === boundingBox.topLeft.x;
-  }
-}
-
-function rightCornerFilter(boundingBox){
-  return function (corner){
-    return corner.x === boundingBox.bottomRight.x;
-  }
-}
-
-function bottomCornerFilter(boundingBox){
-  return function (corner){
-    return corner.y === boundingBox.bottomRight.y;
-  }
-}
-
-
-function findBoundingCorners(boundingBox, corners){
-
-  var topLeft, topRight, bottomLeft, bottomRight;
-
-  var topCorners = corners.filter(topCornerFilter(boundingBox));
-  if(topCorners.length == 2){
-    if(topCorners[0].x < topCorners[1].x){
+  const topCorners = corners.filter(corner => corner.y === boundingBox.topLeft.y);
+  if (topCorners.length === 2) {
+    if (topCorners[0].x < topCorners[1].x) {
       topLeft = topCorners[0];
       topRight = topCorners[1];
     } else {
@@ -56,13 +30,13 @@ function findBoundingCorners(boundingBox, corners){
       topRight = topCorners[0];
     }
     // TODO: find remaining corners, find ordering
-  } else if(topCorners.length == 1){
-    var topCorner = topCorners[0];
-    var leftCorner = corners.find(leftCornerFilter(boundingBox));
-    var rightCorner = corners.find(rightCornerFilter(boundingBox));
-    var bottomCorner = corners.find(bottomCornerFilter(boundingBox));
+  } else if (topCorners.length === 1) {
+    const topCorner = topCorners[0];
+    const leftCorner = corners.find(corner => corner.x === boundingBox.topLeft.x);
+    const rightCorner = corners.find(corner => corner.x === boundingBox.bottomRight.x);
+    const bottomCorner = corners.find(corner => corner.y === boundingBox.bottomRight.y);
 
-    if(leftCorner.y < rightCorner.y){
+    if (leftCorner.y < rightCorner.y) {
       topLeft = leftCorner;
       topRight = topCorner;
       bottomLeft = bottomCorner;
@@ -81,97 +55,95 @@ function findBoundingCorners(boundingBox, corners){
       bottomRight: bottomRight,
     }
   }
-}
+};
 
-function findVerticalIntersectingCorners(boundingBox, corners){
+const findBoundingBox = (corners) => {
+  let minX = Number.MAX_VALUE;
+  let maxX = -1;
+  let minY = Number.MAX_VALUE;
+  let maxY = -1;
 
-}
-
-function findHorizontalIntersectingCorners(boundingBox, corners){
-
-}
-
-function findBoundingBox(corners){
-  var minX = Number.MAX_VALUE;
-  var maxX = -1;
-  var minY = Number.MAX_VALUE;
-  var maxY = -1;
-
-  for(var i=0; i<corners.length; i++) {
-    var corner = corners[i];
-    if(corner.x < minX){minX = corner.x;}
-    if(corner.x > maxX){maxX = corner.x;}
-    if(corner.y < minY){minY = corner.y;}
-    if(corner.y > maxY){maxY = corner.y;}
+  for (let i = 0; i < corners.length; i++) {
+    const corner = corners[i];
+    if (corner.x < minX) {
+      minX = corner.x;
+    }
+    if (corner.x > maxX) {
+      maxX = corner.x;
+    }
+    if (corner.y < minY) {
+      minY = corner.y;
+    }
+    if (corner.y > maxY) {
+      maxY = corner.y;
+    }
   }
 
   return {
-    topLeft: {x: minX, y: minY},
-    bottomRight: {x: maxX, y: maxY}
+    topLeft: { x: minX, y: minY },
+    bottomRight: { x: maxX, y: maxY }
   };
-}
+};
 
-function drawBoundingBox(ctx, bb){
-  var x = bb.topLeft.x;
-  var y = bb.topLeft.y;
-  var height = bb.bottomRight.y - y;
-  var width = bb.bottomRight.x - x;
+const drawBoundingBox = (ctx, bb) => {
+  const x = bb.topLeft.x;
+  const y = bb.topLeft.y;
+  const height = bb.bottomRight.y - y;
+  const width = bb.bottomRight.x - x;
 
   ctx.beginPath();
-  ctx.lineWidth='1';
-  ctx.strokeStyle='red';
+  ctx.lineWidth = '1';
+  ctx.strokeStyle = 'red';
   ctx.rect(x, y, width, height);
   ctx.stroke();
+};
 
-}
-
-function drawPoint(ctx, point, color){
-  var radius = 6;
+const drawPoint = (ctx, point, color) => {
+  const radius = 6;
   ctx.beginPath();
   ctx.arc(point.x, point.y, radius, 0, 2 * Math.PI, false);
   ctx.fillStyle = color;
   ctx.fill();
   ctx.lineWidth = 0;
   ctx.stroke();
-}
+};
 
-function drawCorners(ctx, orderedCorners){
+const drawCorners = (ctx, orderedCorners) => {
   // draw in clockwise order
   drawPoint(ctx, orderedCorners.topLeft, 'red');
   drawPoint(ctx, orderedCorners.topRight, 'green');
   drawPoint(ctx, orderedCorners.bottomRight, 'yellow');
   drawPoint(ctx, orderedCorners.bottomLeft, 'blue');
-}
+};
 
-function findCorners(img){
+const findCorners = (img) => {
   // threshold on difference between intensity of the central pixel
   // and pixels of a circle around this pixel
-  var threshold = 30;
-  jsfeat.fast_corners.set_threshold(threshold);
+  const threshold = 30;
+  const border = 3;
 
-  var corners = [];
-  var border = 3;
+  jsfeat.fast_corners.set_threshold(threshold);
+  const corners = [];
 
   // you should use preallocated keypoint_t array
-  for(var i = 0; i < img.cols*img.rows; ++i) {
-    corners[i] = new jsfeat.keypoint_t(0,0,0,0);
+  for (let i = 0; i < img.cols * img.rows; ++i) {
+    corners[i] = new jsfeat.keypoint_t(0, 0, 0, 0);
   }
 
   // perform detection
   // returns the amount of detected corners
-  var count = jsfeat.fast_corners.detect(img, corners, border);
-
+  const count = jsfeat.fast_corners.detect(img, corners, border);
   return corners.slice(0, count);
-}
+};
 
-function process(ctx, width, height) {
-  var image_data = ctx.getImageData(0, 0, width, height);
-  var gray_img = new jsfeat.matrix_t(width, height, jsfeat.U8_t | jsfeat.C1_t);
+const process = (ctx, width, height) => {
+  const image_data = ctx.getImageData(0, 0, width, height);
+  const gray_img = new jsfeat.matrix_t(width, height, jsfeat.U8_t | jsfeat.C1_t);
   jsfeat.imgproc.grayscale(image_data.data, width, height, gray_img);
 
-  var corners = findCorners(gray_img);
-  var boundingBox = findBoundingBox(corners);
-  var orderedCorners = findBoundingCorners(boundingBox, corners);
+  const corners = findCorners(gray_img);
+  const boundingBox = findBoundingBox(corners);
+  const orderedCorners = findBoundingCorners(boundingBox, corners);
   console.log('corners', corners);
   console.log('bounding box', boundingBox);
   console.log('orderedCorners', orderedCorners);
@@ -181,7 +153,7 @@ function process(ctx, width, height) {
   ctx.putImageData(image_data, 0, 0);
   drawBoundingBox(ctx, boundingBox);
   drawCorners(ctx, orderedCorners);
-}
+};
 
 export default (ctx, width, height) => {
   drawImageOnCanvas(ctx);
