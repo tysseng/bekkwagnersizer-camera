b628d6bb1010f4946ab9b517700b317c4dcf4f73
Joakim Tysseng <joakim@tysseng.no>
2018-05-30 19:09:52 +0200

    Fixed error handling.

--- src/utils/promises.js
+++ src/utils/promises.js
@@ -7,12 +7,17 @@ export const timeout = (ms) => {
 
 export const abortable = (funcToAsync) => {
   return new Promise((resolve, reject) => setTimeout(() => {
-    const result = funcToAsync();
-    if(isRunning()){
-      resolve(result);
-    } else {
-      logger.error('- ABORTED, stop was pressed')
-      reject('ABORT');
+    try{
+      const result = funcToAsync();
+      if(isRunning()){
+        resolve(result);
+      } else {
+        logger.error('- ABORTED, stop was pressed');
+        reject('ABORT');
+      }
+    } catch (err) {
+      reject(err)
     }
+
   }, 0));
 };
