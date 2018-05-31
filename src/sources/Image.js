e961eb2c7390326c03a9f7725bd421e6472ad210
Joakim Tysseng <joakim@tysseng.no>
2018-05-31 07:58:19 +0200

    Removed image dependency to prevent problems after cloning git repo on a new machine

--- src/sources/Image.js
+++ src/sources/Image.js
@@ -1,13 +1,14 @@
 // @flow
 import React from 'react';
-import imageToProcess from '../assets/images/IMG_6304.jpg';
+//import imageToProcess from '../assets/images/IMG_6304.jpg';
 
 class Image extends React.Component {
 
+  // TODO: Uncomment image import to be able to use static image.
   render() {
     return (
       <div>
-        <img ref='image' src={imageToProcess} alt="There's no alternative!"/>
+        {/*<img ref='image' src={imageToProcess} alt="There's no alternative!"/>*/}
       </div>
     );
   }
