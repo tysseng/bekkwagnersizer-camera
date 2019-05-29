46170c510981353d7338f0e1861337784640fb11
Joakim Tysseng <joakim@tysseng.no>
2019-05-29 13:01:29 +0200

    Updated led colors to cyan on while working and magenta on failure.

--- src/communication/statusIndicator.js
+++ src/communication/statusIndicator.js
@@ -8,7 +8,7 @@ const success = () => {
 };
 
 const failure = () => {
-  fetch('http://localhost:3007/blink?r=255&g=0&b=100&duration=1000&times=4', {
+  fetch('http://localhost:3007/blink?r=255&g=0&b=255&duration=1000&times=4', {
     method: 'GET',
   });
   logger.info('FAILURE!');
@@ -22,7 +22,7 @@ const colorsCalibrated = () => {
 };
 
 const processing = () => {
-  fetch('http://localhost:3007/spin?r=255&g=255&b=255&speed=20', {
+  fetch('http://localhost:3007/spin?r=0&g=255&b=255&speed=20', {
     method: 'GET',
   });
   logger.info('PROCESSING');
